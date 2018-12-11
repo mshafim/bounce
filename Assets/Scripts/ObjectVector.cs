615b5f0642404e27d199432840f38bdb341f4785
MOHAMMED SHAFIM <2032517@CRL-1311A-11.local>
2018-12-11 14:24:55 -0500

    edited objectvector script

---
+++ Assets/Scripts/ObjectVector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVector : MonoBehaviour {
+	Vector2 object_vector;
+
+	public Collider2D objectCollider;
+	public Collider2D rampCollider;
+
+	private List<bool> Truths = new List<bool>();
+
+	// Use this for initialization
+	void Start () {
+		objectCollider = GameObject.FindGameObjectWithTag("Object Sprite").GetComponent<Collider2D>();
+		rampCollider = GameObject.FindGameObjectWithTag("Ramp Sprite v2").GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (objectCollider.IsTouching(rampCollider))
+			Truths.Add(true);
+
+		Debug.Log(Truths.Count);
+	}
+}
