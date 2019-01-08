fc4bc43a6eaa03c575094464dd856b85c0ab8919
MOHAMMED SHAFIM <19mshafim@gmail.com>
2019-01-07 22:17:55 -0500

    added spacebar to roll

---
+++ Assets/Scripts/RollBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollBall : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space"))
+        {
+             rb.constraints = RigidbodyConstraints2D.None;
+        }
+	}
+}
