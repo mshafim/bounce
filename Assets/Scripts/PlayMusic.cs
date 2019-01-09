828395d5b1894ffb1fc376b5cd572e16893bc1d4
MOHAMMED SHAFIM <19mshafim@gmail.com>
2019-01-08 21:49:21 -0500

    added music player

--- Assets/Scripts/PlayMusic.cs
+++ Assets/Scripts/PlayMusic.cs
@@ -3,8 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayMusic : MonoBehaviour {
+    public AudioSource adSource;
+	public AudioClip[] adClips;
+	private IEnumerator coroutine;
+
+	IEnumerator playAudioSequentially()
+	{
+		Debug.Log("working");
+	    yield return null;
+
+	    //1.Loop through each AudioClip
+	    for (int i = 0; i < adClips.Length; i++)
+	    {
+	        //2.Assign current AudioClip to audiosource
+	        adSource.clip = adClips[i];
+
+	        //3.Play Audio
+	        adSource.Play();
+
+	        //4.Wait for it to finish playing
+	        while (adSource.isPlaying)
+	        {
+	            yield return null;
+	        }
+
+	        //5. Go back to #2 and play the next audio in the adClips array
+	    }
+	}
 
 	void Awake() {
-         DontDestroyOnLoad(transform.gameObject);
+		coroutine = playAudioSequentially();
+		StartCoroutine(coroutine);
+        DontDestroyOnLoad(transform.gameObject);
      }
+
+	
 }
