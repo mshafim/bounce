0cb9eb0d3794121f07f43056ca02c337f662fc9c
MOHAMMED SHAFIM <19mshafim@gmail.com>
2019-01-09 01:51:25 -0500

    added height input

--- Assets/Scripts/RestartLevel.cs
+++ Assets/Scripts/RestartLevel.cs
@@ -9,12 +9,17 @@ public class RestartLevel : MonoBehaviour {
 	private Rigidbody2D rb;
 	public GameObject restart_button;
 	public GameObject quit_button;
+	public GameObject height;
+	// public GameObject boost;
 
 
 	// Use this for initialization
 	void Start () {
 		restart_button.SetActive(false);
 		quit_button.SetActive(false);
+
+		height.SetActive(true);
+		// boost.SetActive(true);
 
 		t = GetComponent<Transform>();
 		rb = GetComponent<Rigidbody2D>();
@@ -24,12 +29,18 @@ public class RestartLevel : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		if (Input.GetKeyDown("space"))
+        {
+			height.SetActive(false);
+			// boost.SetActive(false);
+		}
+
 		if (t.position[0] > -5.13 && y == t.position[1])
 		{
 			count++;
 		}
 
-		if (count == 200)
+		if (count == 1000)
 		{
 			rb.constraints = RigidbodyConstraints2D.FreezePositionX;
 			restart_button.SetActive(true);
---
+++ Assets/Scripts/TextInput.cs
@@ -0,0 +1,48 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class TextInput : MonoBehaviour
+// {
+	
+// 	public InputField height_input;
+
+//     public void ChangeHeight () {
+//     	float height = Convert.ToSingleheight_input.text;
+//     	t = GetComponent<Transform>();
+//     	t.transform.position = new Vector2(t.position[0], height);
+//     }
+
+
+// }
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextInput : MonoBehaviour {
+	private TMP_InputField input;
+	private Transform t;
+
+    void Start ()
+    {
+    	t = GameObject.FindWithTag("Moving").GetComponent<Transform>();
+        input = GetComponent<TMP_InputField>(); 
+        var se = new TMP_InputField.SubmitEvent();
+        se.AddListener(ChangeHeight);
+        input.onEndEdit = se;
+
+        //or simply use the line below, 
+        // input.onEndEdit.AddListener(SubmitName);  // This also works
+    }
+
+    private void ChangeHeight(string height_input)
+    {
+    	float height = System.Convert.ToSingle(height_input);
+    	t.transform.position = new Vector2(t.position[0], height);
+    }
+}
+
