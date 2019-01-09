718a832ef0b86877d222ca67fed8f57f3978e0f8
MOHAMMED SHAFIM <19mshafim@gmail.com>
2019-01-09 00:28:48 -0500

    added restart and quit functions

--- Assets/Scripts/RestartMenu.cs
+++ Assets/Scripts/RestartMenu.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class RestartMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class RestartMenu : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void RestartGame ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void QuitGame ()
+	{
+		Application.Quit();
+	}
+
 }
